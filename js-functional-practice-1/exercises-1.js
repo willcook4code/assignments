b545e7b4584701c39f46baf177cae36626f83996
Will Cook <wellsee83@gmail.com>
2016-03-11 22:07:08 -0600

    Completed all exercises

--- js-functional-practice-1/exercises-1.js
+++ js-functional-practice-1/exercises-1.js
@@ -13,13 +13,17 @@
 // 0. write your own forEach() that takes two arguments: an array, and a callback
 
 function forEach(array, callback){
-    // YOUR CODE HERE
+ 
+    for (var i=0; i < array.length; i++) {
+        callback(array[i]);
+    }
 
 }
 
 // testing your code with console.assert
 var total = 1;
 forEach([1, 2, 3, 4], function(a){ total *= a; });
+//console.log(total === 24);
 // and finally assert; if this fails, the program stops
 console.assert(total === 24);
 
@@ -29,11 +33,18 @@ console.assert(total === 24);
 function sum(){
     // parse arguments into an array
     var args = [].slice.call(arguments);
-
+    var total = 0;
     // .. do something with each element of args
     // YOUR CODE HERE
+    for (var i=0; i < args.length; i++) {
+       
+        total+=(args[i]);
+        console.log(args);
+        console.log(total);
+    }
+    return total;
 }
-
+console.log(sum());
 console.assert( sum(1, 2, 3, 4, 5) === 15 )
 
 // 2. calculate the average of numbers (returns the average (A NUMBER))
@@ -41,9 +52,16 @@ console.assert( sum(1, 2, 3, 4, 5) === 15 )
 function average(){
     // parse arguments into an array
     var args = [].slice.call(arguments);
-
+    var total = 0;
     // .. do something with each element of args
     // YOUR CODE HERE
+    for (var i=0; i < args.length; i++) {
+   
+    var ans = (total+=(args[i]))/args.length;
+    console.log(args);
+    console.log(total);
+    }
+    return ans;
 }
 
 console.assert( average(2, 4, 6, 8) === 5 )
@@ -53,24 +71,40 @@ console.assert( average(2, 4, 6, 8) === 5 )
 function largest(){
     // parse arguments into an array
     var args = [].slice.call(arguments);
-
+    var base = 0;
     // .. do something with each element of args
     // YOUR CODE HERE
+    for (var i=0; i < args.length; i++) 
+        if (args[i] > base) {
+            base = args[i];  
+    }
+    console.log(base);
+    return(base);
 }
 
-console.assert( largest(2, 4, 6, 8) === 8 )
+
+console.assert( largest(2, 8, 6, 4) === 8 )
 
 // 4. find the longest string of the inputs (returns the longest input (A STRING))
 
 function longest(){
     // parse arguments into an array
     var args = [].slice.call(arguments);
+    var base = 0;
 
     // .. do something with each element of args
     // YOUR CODE HERE
+    for (var i=0; i < args.length; i++) {
+        if (args[i].length > base) {
+            base = args[i].length;
+            var newLongest = args[i];
+            console.log(args);
+        }
+    }
+    return(newLongest);
 }
 
- console.assert( longest("this", "is", "a", "awesome", "function") === "function" );
+console.assert( longest("this", "is", "a", "awesome", "function") === "function" );
 
 /**
  * PART II
@@ -82,27 +116,71 @@ function longest(){
  */
 
 // .sort()
+var soda = ["Dr. Pepper", "Sprite", "Coke", "7-Up", "Fanta"];
+console.log(soda.sort());
+console.assert( soda.sort()[0] === "7-Up");
 
 // .concat()
+var soda = ["Dr. Pepper", "Sprite", "Coke", "7-Up", "Fanta"];
+var tea = ["Black", "Earl Grey", "Iced"];
+var drinks = tea.concat(soda);
+console.assert(drinks[5] === "Coke");
 
 // .indexOf()
+var soda = ["Dr. Pepper", "Sprite", "Coke", "7-Up", "Fanta"];
+console.assert(soda.indexOf("Fanta") === 4);
 
 // .split()
+var drinks = 'I like tea and soda in my mouth when I have thirst.'
+var drinksArray = drinks.split(' ');
+console.log(drinksArray);
+console.assert(drinksArray[7] === 'mouth');
 
 // .join()
+var manyWords = ['All', 'these', 'words', 'are', 'one.'];
+console.log(manyWords.join(''));
+console.assert(manyWords.join('') === 'Allthesewordsareone.');
 
 // .pop()
+var soda = ["Dr. Pepper", "Sprite", "Coke", "7-Up", "Fanta"];
+console.assert(soda.pop() === "Fanta");
 
 // .push()
+var soda = ["Dr. Pepper", "Sprite", "Coke", "7-Up", "Fanta"];
+console.assert(soda.push("Mountain Dew", "Pepsi") === 7);
 
 // .slice()
+var soda = ["Dr. Pepper", "Sprite", "Coke", "7-Up", "Fanta"];
+var someSodas = soda.slice(0, 3)
+console.assert(someSodas[2] === "Coke");
 
 // .splice()
+var soda = ["Dr. Pepper", "Sprite", "Coke", "7-Up", "Fanta"];
+soda.splice(1, 3, "Mountain Dew", "Pepsi");
+console.log(soda[3]);
+console.assert(soda[3] === "Fanta");
 
 // .shift()
+var soda = ["Dr. Pepper", "Sprite", "Coke", "7-Up", "Fanta"];
+console.assert(soda.shift() === 'Dr. Pepper');
 
 // .unshift()
-
+var soda = ["Dr. Pepper", "Sprite", "Coke", "7-Up", "Fanta"];
+console.assert(soda.unshift('Mountain Dew', 'Pepsi') === 7);
 // .filter()
+var soda = ["Dr. Pepper", "Sprite", "Coke", "7-Up", "Fanta"];
+function quitSprite(drink) {
+    return drink !== "Sprite";
+}
+console.assert(soda.filter(quitSprite)[2] === "7-Up");
 
 // .map()
+var soda = ["Dr. Pepper", "Sprite", "Coke", "7-Up", "Fanta"];
+function tryIt(drink) {
+    return 'Sip on a ' + drink;
+}
+console.assert(soda.map(tryIt)[2] === 'Sip on a Coke');
+
+
+
+
